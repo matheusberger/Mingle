253e7b5b2eb9f1ba9a766603e6dd0a932ac7ee77
Matheus Berger <matheuscberger@gmail.com>
2020-08-03 15:44:06 -0300

    RemoteControlled players should now animate correctly

--- Assets/Scripts/RemoteController.cs
+++ Assets/Scripts/RemoteController.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class RemoteController : MonoBehaviour, IPlayerListener
 {
     private CharacterController characterController;
+    private Animator anim;
 
     private void Start()
     {
         characterController = GetComponent<CharacterController>();
+        anim = GetComponent<Animator>();
     }
 
     public void MovePlayer(Vector2 target)
@@ -16,7 +18,19 @@ public class RemoteController : MonoBehaviour, IPlayerListener
         Vector3 target3D = new Vector3(target.x, transform.position.y, target.y);
         if(characterController != null)
         {
-            characterController.Move((target3D - transform.position) * Time.deltaTime);
+            //Calculate the Input Magnitude
+            var speed = target.sqrMagnitude;
+
+            //Physically move player
+            if (speed > 0.1f)
+            {
+                anim.SetFloat("Blend", speed, 0.3f, Time.deltaTime);
+                characterController.Move((target3D - transform.position) * Time.deltaTime);
+            }
+            else if (speed < 0.1f)
+            {
+                anim.SetFloat("Blend", speed, 0.15f, Time.deltaTime);
+            }
         }
     }
 
